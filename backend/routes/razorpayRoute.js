d7997b9ee54452ed709f28a21911a4c1ec6a0f2f
Sandeep Yadav <sandeepashokyadav793@gmail.com>
2026-06-10 08:02:44 +0530

    Fix: Case sensitivity mismatch in razorpay controller import path

--- backend/routes/razorpayRoute.js
+++ backend/routes/razorpayRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRazorpayOrder, verifyRazorpayPayment } from "../controllers/razorpayController.js";
+import { createRazorpayOrder, verifyRazorpayPayment } from "../controllers/razorPayController.js";
 import { authware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
